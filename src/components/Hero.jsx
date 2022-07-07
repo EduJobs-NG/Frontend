f231b5f8b4a7b7043fa59b28a8ce84a81e11d95d
Oluwaniphise <oduyaleenoch@gmail.com>
2022-07-07 10:46:53 +0100

    added hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div className=' container mx-auto'></div>
+    <section className='relative bg-hero h-full py-[150px] lg:py-[200px] max-h-[700px] bg-top bg-cover bg-no-repeat'>
+      <div className='container mx-auto flex flex-col justify-center '>
+        <h2 className='title text-[35px] font-heading lg:text-[66px] z-[999] font-[700] text-white max-w-[600px]'>Have Great Jobs
+          Find You!</h2>
+        <p className='subtitle text-[25px] z-[999] lg:text-[36px] text-white max-w-[600px] '>
+          Find Jobs that match Your interests
+          on EduJobs NG
+        </p>
+      </div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#02378B] opacity-40 ">
+      </div>
+
+    </section>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {navigation} from '../data';
+
+export const Navbar = () => {
+  return (
+    <section className='bg-[#02378B]'>
+        <div className="container mx-auto">
+
+        </div>
+
+    </section>
+  )
+}
