52357e544d1bda45bef4b65df1b1eb3da7f0980d
Oluwaniphise <oduyaleenoch@gmail.com>
2022-07-16 07:32:40 +0100

    edited loginform

--- src/components/Forms/LoginFormUI.jsx
+++ src/components/Forms/LoginFormUI.jsx
@@ -6,3 +6,32 @@ export const LoginFormUI = () => {
     <FormInputBox label="First Name" />
   )
 }
+
+
+// onSubmit: (values) => {
+//   // const {} = values;
+//   const REST_API_URL = "http://edujobsng.herokuapp.com/api/v1/auth/users/";
+//   fetch(REST_API_URL, {
+//       method: 'post',
+      
+//       headers: {
+//           'Content-Type': 'application/json',
+//           // 'Access-Control-Allow-Origin': 'http://127.0.0.1:3000/'
+//         },
+//       body: JSON.stringify(values)
+//   }).then(response => {
+//       if (response.ok) {
+//           return response.json();
+//       } else {
+//           // HANDLE ERROR
+//           throw new Error('Something went wrong');
+          
+//       }
+//   }).then(data => {
+//       // HANDLE RESPONSE DATA
+//       console.log(data)
+//   }).catch((error) => {
+//       // HANDLE ERROR
+//       console.log(error)
+//   });
+}
