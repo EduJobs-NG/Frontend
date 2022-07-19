7ccc52564fae7fdc2c731000a44f1f9bffae1d10
Oluwaniphise <oduyaleenoch@gmail.com>
2022-07-19 04:27:50 +0100

    working on verifyaccount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Home } from './components/Home';
 import {Navbar} from './components/Navbar';
 import {RegisterFormUI} from './components/Forms/RegisterFormUI';
 import {LoginFormUI} from './components/Forms/LoginFormUI';
+import {VerifyAccount} from './components/Forms/VerifyAccount'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" exact  element={<Home />} />
       <Route path="/login" element={<LoginFormUI />}/>
       <Route path="/register" element={<RegisterFormUI />}/>
+      <Route path="/activate/:NQ/:token" element={<VerifyAccount />}/>
       
 
       </Routes>
---
+++ src/components/Forms/VerifyAccount.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const VerifyAccount = () => {
+  return (
+    <div>Hey your account has been verified. Ku ori ire</div>
+  )
+}
