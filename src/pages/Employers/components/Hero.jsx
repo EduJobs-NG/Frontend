dfb88a05927de92e3e489dc4fd2579b13f351b69
Oluwaniphise <oduyaleenoch@gmail.com>
2022-08-24 16:15:20 +0100

    working on employers landing page

--- src/pages/Employers/components/Hero.jsx
+++ src/pages/Employers/components/Hero.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export const Hero = () => {
   return (
     <>
-    <section className='relative bg-employers h-full py-[150px] lg:py-[200px] max-h-[700px] bg-top bg-cover bg-no-repeat'>
-      <div className='container mx-auto flex flex-col justify-center '>
-        <h2 className='title text-[35px] font-heading lg:text-[66px] z-[999] font-[700] text-white max-w-[600px]'>Have Great Jobs
-          Find You!</h2>
-        <p className='subtitle text-[25px] z-[999] lg:text-[36px] text-white max-w-[600px] '>
-          Find Jobs that match Your interests
-          on EduJobs NG
-        </p>
-      </div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#02378B] opacity-40 ">
-      </div>
-
-    </section>
-    
+      <section className="relative bg-employers h-full py-[150px] lg:py-[200px] max-h-[700px] bg-top bg-cover bg-no-repeat">
+        <div className="container mx-auto flex flex-col justify-center ">
+          <h2 className="title text-[35px] font-heading lg:text-[66px] z-[999] font-[700] text-white max-w-[600px]">
+            Hire Top Talents faster than ever!
+          </h2>
+          <p className="subtitle text-[25px] z-[999] lg:text-[36px] text-white max-w-[700px] ">
+            Save time and money on recruitment with EduJobs matchmaking
+            technology and Candidates' assessement.
+          </p>
+        </div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-blue opacity-60 "></div>
+      </section>
     </>
-
-  )
-}
+  );
+};
