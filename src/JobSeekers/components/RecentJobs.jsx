74e12d037d3fd289e3f27621a0bae200572f5c1f
Oluwaniphise <oduyaleenoch@gmail.com>
2022-08-29 15:35:39 +0100

    working on job application

---
+++ src/JobSeekers/components/RecentJobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const RecentJobs = () => {
+  return (
+    <>
+    <section className='bg-[#f5f5f5]'>
+        <div className='container mx-auto'>
+            <hr className='text-[#d9d9d9]' />
+        </div>
+
+    </section>
+    </>
+  )
+}
