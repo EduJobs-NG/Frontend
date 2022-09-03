f8956e4dbfd4a7e494620301c6d965deae07fc66
Oluwaniphise <oduyaleenoch@gmail.com>
2022-09-03 15:48:18 +0100

    working on jobs

--- src/JobSeekers/components/RecentJobs.jsx
+++ src/JobSeekers/components/RecentJobs.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 export const RecentJobs = () => {
+  const [viewMore, setViewMore] = useState(false)
+  const handleView = () =>{
+    setViewMore(!viewMore)
+  }
+  const jd = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem autem odit suscipit quae consectetur. Provident, commodi sint nobis repellendus iure doloremque reiciendis ut hic ratione beatae perferendis veritatis ipsam soluta excepturi sed. Delectus, consectetur recusandae praesentium eligendi molestias quaerat ea, blanditiis ullam obcaecati provident a porro. Sunt numquam architecto inventore!'
   return (
     <>
     <section className='bg-[#f5f5f5]'>
         <div className='container mx-auto'>
             <hr className='text-[#d9d9d9]' />
             <h2 className='text-blue my-[1rem] font-[700] text-[1.5rem]'>Recent Jobs</h2>
-            <div className='relative ml-[1rem]  py-[1.2rem] border bg-white border-[#d9d9d9] px-[1.2rem] rounded-[20px]'>
+            <div className='relative ml-[1rem] my-[rem]  py-[1.2rem] border bg-white border-[#d9d9d9] px-[1.2rem] rounded-[20px]'>
 
               <h2 className='font-[700] text-[1.2rem]'>Chemistry Teacher</h2>
               <p className='font-[500]'>Stars College</p>
               <p className='font-[500]'>Ikorodu, Lagos.</p>
-              <p className='mt-[0.5rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur placeat, non dicta ratione, 
-                et voluptatem eaque ducimus nemo optio expedita corporis, rerum ab magni sunt consectetur. 
-                Quis exercitationem similique temporibus ratione dolorem modi iusto, voluptatibus quae sunt
-                 odio sequi, neque, rem autem aliquam laudantium mollitia vero consequatur ducimus vel asperiores!
+              <p className='mt-[0.5rem] mb-[1.2rem]'>
+               {viewMore ? `${jd}` : `${jd.substring(0, 250)}...` } 
                   </p>
-                  <p className='mt-[1rem]'>5 hours ago</p>
-                  <p className='cursor-pointer font-[600] absolute text-blue right-5 bottom-[1rem]'>View more</p>
+                  <p className='absolute  left-5 bottom-[0.5rem]'>5 hours ago</p>
+                  <p onClick={handleView} className='cursor-pointer font-[600] absolute text-blue right-5 bottom-[0.5rem]'>{viewMore ? 'View Less':'View More'}</p>
 
+                  {viewMore && (
+                    <div className='my-[1rem]'>
+                      <h1 className='font-[700]'>Requirements</h1>
+                      <ol className='list-style'>
+                        <li>lorem ipsum dolor sit amer consectetur elit</li>
+                        <li>lorem ipsum dolor sit amer consectetur elit</li>
+                        <li>lorem ipsum dolor sit amer consectetur elit</li>
+                        <li>lorem ipsum dolor sit amer consectetur elit</li>
+                        <li>lorem ipsum dolor sit amer consectetur elit</li>
+                        <li>lorem ipsum dolor sit amer consectetur elit</li>
+                      </ol>
+                    </div>
+                  )}
             </div>
         </div>
 
