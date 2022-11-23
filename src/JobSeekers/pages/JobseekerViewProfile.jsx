b46a76a9fbd4acdb4fd98703963f0e8944894a63
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-11-24 00:31:37 +0100

    modified the user profile page

--- src/JobSeekers/pages/JobseekerViewProfile.jsx
+++ src/JobSeekers/pages/JobseekerViewProfile.jsx
@@ -6,10 +6,15 @@ import { Footer } from '../../components/Footer';
 import photo from '../../assets/asideprofile.svg';
 import AuthContext from '../../context/AuthContext';
 import { JobseekerNavbar as Navbar } from '../components/JobseekerNavbar';
+import { useEffect } from 'react';
 
 export const Profile = () => {
     // hooks
     const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        console.table(user);
+    },[]);
 
     return <>
         <Navbar user={user} />
@@ -43,7 +48,7 @@ export const Profile = () => {
                         <div className="flex flex-col grow basis-52">
                             <label htmlFor="middle_name" className="font-thinbold capitalize text-grey" children='middle name' />
                             <span className="mx-1 shrink grow">
-                                <input name="middle_name" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.user?.middle_name} />
+                                <input name="middle_name" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.middle_name} />
                             </span>
                         </div>
                         <div className="flex flex-col grow basis-52">
@@ -61,7 +66,7 @@ export const Profile = () => {
                         <div className="flex flex-col grow basis-52">
                             <label htmlFor="phone_number" className="font-thinbold capitalize text-grey" children='phone number' />
                             <span className="mx-1 shrink grow">
-                                <input name="phone_number" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.user?.phone_number} />
+                                <input name="phone_number" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.phone_number} />
                             </span>
                         </div>
                         <div className="flex flex-col grow basis-52">
@@ -77,15 +82,17 @@ export const Profile = () => {
                         <h3 className="capitalize font-bold text-md">professional information</h3>
                         <Link to='/dashboard/profile/edit?tab=1'><AiFillEdit className='w-8' /></Link>
                     </header>
-                    <div className="grow-1 w-full flex flex-col items-start">
-                        <h3 className="font-thin text-sm leading-[1em]">Bsc Elect/Elect</h3>
-                        <span className="font-thin text-sm leading-[1em]">University of Lagos</span>
-                        <span className="font-thin text-sm leading-[1em]">Aug. 2016 - June 2021</span>
-                        <span className="font-thin text-sm leading-[1em]">First Class</span>
-                        <p className="leading-[1em] text-md">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, dicta necessitatibus? Illo iure nulla placeat tempore mollitia odit ipsa, corporis repellendus omnis rerum aliquam exercitationem cupiditate rem saepe nemo sequi itaque similique perspiciatis aut doloremque voluptates ut? Omnis, debitis itaque!
-                        </p>
-                    </div>
+                    {user?.professional_info?.map(item => (
+                    <div key={item?.id} className="grow-1 w-full flex flex-col items-start my-2">
+                    <h3 className="font-thin text-sm leading-[1em]">{item?.degree}</h3>
+                    <span className="font-thin text-sm leading-[1em]">{item?.school_name}</span>
+                    <span className="font-thin text-sm leading-[1em]">{item?.start_of_education} - {item?.end_of_education}</span>
+                    <span className="font-thin text-sm leading-[1em]">{item?.grade}</span>
+                    <p className="leading-[1em] text-md">
+                        {item?.study_summary}
+                    </p>
+                        </div>
+                    ))}
                 </div>
                 <div className="flex flex-col w-full max-w-[52em] rounded-lg border-gray-400 border p-4">
                     <header className="w-full flex items-center justify-between">
@@ -112,19 +119,19 @@ export const Profile = () => {
                         <div className="flex flex-col grow basis-52">
                             <label htmlFor="first_name" className="font-thinbold capitalize text-grey" children='first name' />
                             <span className="mx-1 shrink grow">
-                                <input name="first_name" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={'Faith'} />
+                                <input name="first_name" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.contact_info?.next_kin_fname} />
                             </span>
                         </div>
                         <div className="flex flex-col grow basis-52">
                             <label htmlFor="last_name" className="font-thinbold capitalize text-grey" children='last name' />
                             <span className="mx-1 shrink grow">
-                                <input name="last_name" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={'Daniel'} />
+                                <input name="last_name" type="text" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.contact_info?.next_kin_lname} />
                             </span>
                         </div>
                         <div className="flex flex-col grow basis-52">
-                            <label htmlFor="phone_number" className="font-thinbold capitalize text-grey" children='last name' />
+                            <label htmlFor="phone_number" className="font-thinbold capitalize text-grey" children='phone number' />
                             <span className="mx-1 shrink grow">
-                                <input name="phone_number" type="nutextmber" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={'+234 80 602 302 85'} />
+                                <input name="phone_number" type="nutextmber" disabled className='rounded-md text-sm p-2 w-full border border-grey' value={user?.contact_info?.next_kin_phone} />
                             </span>
                         </div>
                     </div>
