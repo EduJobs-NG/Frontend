935284ef8e378554c85b2939a50b11206739e252
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2022-11-25 18:09:12 +0100

    merged final to main

--- src/layouts/index.js
+++
@@ -1,6 +0,0 @@
-export { default as main } from './main';
-export { default as auth } from './auth';
-export { default as jobpost } from './jobpost';
-export { default as employer } from './employer';
-export { default as jobseeker } from './jobseeker';
-export { default as dashboard } from './edashboard';
