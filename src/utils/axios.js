081c456bd2720a9c2673426faba0288b5cfe982a
Oduyale Enoch <oduyaleenoch@gmail.com>
2023-01-05 13:26:32 -0800

    changed API URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import { token } from './storage';
 
 /* Creating an axios instance with the base url and the headers. */
 const api = axios.create({
-    baseURL: 'https://edujobsng.up.railway.app/api/v1',
+    baseURL: 'https://api.edujobsng.com/api/v1/',
     headers: {
         Authorization: token.key,
         "Content-Type": "application/json",
